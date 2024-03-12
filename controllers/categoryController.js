1cc097d8f58ec39fb6a42c634df1997ba779c592
adeearyaa <adeearyaa2013@gmail.com>
2024-03-12 14:32:36 +0800

    Add controllers for the models with skeleton functions

---
+++ controllers/categoryController.js
@@ -0,0 +1,46 @@
+const Category = require("..models/categories")
+const asyncHandler = require("express-async-handler")
+
+// going to make a list of function that the api endpoint will be calling
+
+
+// displays all the categories that we have 
+exports.categoryList = asyncHandler(async(req,res,next)=> {
+
+});
+
+// displays the info about each particular category
+exports.categoryDetail = asyncHandler(async(req,res,next)=> {
+
+});
+
+// displays the form that will allow a new category to be added
+exports.categoryCreateGet = asyncHandler(async(req,res,next)=> {
+
+});
+
+// handles the form submission and updated the database
+exports.categoryCreatePost = asyncHandler(async(req,res,next)=> {
+
+});
+
+// displays the category delete form on get
+exports.categoryDeleteGet = asyncHandler(async(req,res,next)=> {
+
+});
+
+// handles form submission of deleting a category
+exports.categoryDeletePost = asyncHandler(async(req,res,next)=> {
+
+});
+
+// displays the form for updating a category
+exports.categoryUpdateGet = asyncHandler(async(req,res,next)=> {
+
+});
+
+//handles the form submission for updating a category
+exports.categoryUpdatePost = asyncHandler(async(req,res,next)=> {
+
+});
+
